4077ee8fa11dd851e119751642f83ca5b1e06db3
Vitor Cremonez <vitorcremonez@gmail.com>
2018-02-02 14:42:10 -0200

    Making transition between 2 values on number update.

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import '../styles/App.css';
 import TransitionNumber from "./TransitionNumber";
+import BadgeNotification from 'react-badge-notification';
 
 class App extends Component {
     constructor(props) {
@@ -10,12 +11,28 @@ class App extends Component {
         };
     }
 
+    randomNumber() {
+        this.setState({
+            myNumber: this.state.myNumber+100//Math.round(Math.random() * 10000000)
+        })
+    }
+
     render() {
         return (
             <h1>
-                <TransitionNumber>
+                { this.state.myNumber }
+                <br/>
+                <TransitionNumber duration={1000} points={60}>
                     { this.state.myNumber }
                 </TransitionNumber>
+                <br/>
+                <button onClick={() => this.randomNumber()}>sort number</button>
+                <BadgeNotification quantity={10}>
+                    olá mundo
+                </BadgeNotification>
+                <div className="badge-notification">
+                    ops
+                </div>
             </h1>
         );
     }
--- src/components/TransitionNumber.js
+++ src/components/TransitionNumber.js
@@ -1,10 +1,46 @@
 import React, {Component} from 'react';
 
 class TransitionNumber extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            number: this.props.children,
+        };
+    }
+
+    componentWillUpdate(newProps) {
+        let a = this.props.children;
+        let b = newProps.children;
+        if (a !== b) {
+            for (let x=0; x<=1; x+=(1/this.props.points)) {
+                //alert(x);
+                setTimeout(() => {
+                        this.setState({
+                            number: Math.round(a + x*(b-a)),
+                        });
+                    },
+                    this.f(x) * this.props.duration
+                );
+                setTimeout(() => {
+                        this.setState({
+                            number: Math.round(b),
+                        });
+                    },
+                    this.props.duration
+                );
+            }
+        }
+    }
+
+    f(x) {
+        return Math.sin((x-1)*Math.PI/2) + 1;
+        return x;
+    }
+
     render() {
         return (
             <span>
-                { this.props.children }
+                { this.state.number }
             </span>
         );
     }
